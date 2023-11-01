78ad41a322e26ae137292df92aa8271aa34ea157
Ushnish Tapaswi <126304736+plushexe351@users.noreply.github.com>
2023-11-01 06:04:07 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # FlappyBird
-A Flappy Bird game made using HTML, CSS and JavaScript.
+
+Relive your childhood with this Flappy Bird game made using HTML, CSS and JavaScript.
+
+P.S - Feel free to send pull requests if it can be better.
+
+Enjoy !
+
+## Live Preview
+
+Github Pages :
---
+++ script.js
@@ -0,0 +1,181 @@
+var canvas = document.getElementById('gameCanvas');
+var ctx = canvas.getContext('2d');
+
+var bird = new Image();
+bird.src = 'images/bird.png'; // Replace with the path to your bird image
+
+var birdX = canvas.width / 2 - 30; // Adjusted for centering horizontally
+var birdY = canvas.height / 2 - 22.5; // Adjusted for centering vertically
+var birdWidth = 60;
+var birdHeight = 45;
+var gravity = 0.5;
+var jump = 8.5;
+var velocity = 0;
+var gameStarted = false;
+var gameOver = false;
+var stopObstacles = false;
+
+var obstacles = [];
+var gap = 150;
+var obstacleSpeed = 2;
+
+var pipeTop = new Image();
+pipeTop.src = 'images/pipe_top.png'; // Replace with the path to your pipe top image
+var pipeBottom = new Image();
+pipeBottom.src = 'images/pipe_bottom.png'; // Replace with the path to your pipe bottom image
+
+var groundHeight = 30; // Adjust ground height as needed
+
+
+
+document.addEventListener('keydown', function (event) {
+    if (event.code === 'Space') {
+        if (gameOver) {
+            reset();
+        } else if (!gameStarted) {
+            gameStarted = true;
+        } else {
+            velocity = -jump;
+        }
+    }
+});
+
+document.addEventListener('click', () => {
+    if (gameOver) {
+        reset();
+    } else if (!gameStarted) {
+        gameStarted = true;
+    } else {
+        velocity = -jump;
+    }
+})
+
+bird.onload = function () {
+    drawBird();
+    draw();
+};
+
+function reset() {
+    birdY = canvas.height / 2 - 22.5; // Adjusted for centering vertically
+    velocity = 0;
+    obstacles = [];
+    gameStarted = false;
+    gameOver = false;
+    stopObstacles = false;
+}
+
+function resizeCanvas() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    birdX = canvas.width / 2 - 30; // Adjusted for centering horizontally during window resize
+    birdY = canvas.height / 2 - 22.5; // Adjusted for centering vertically during window resize
+
+
+}
+
+window.addEventListener('resize', function () {
+    resizeCanvas();
+    draw();
+    reset();
+
+
+});
+
+function checkCollision(birdX, birdY, birdWidth, birdHeight, obstacle) {
+    if (
+        birdX < obstacle.x + obstacle.width &&
+        birdX + birdWidth > obstacle.x &&
+        (birdY < obstacle.top || birdY + birdHeight > obstacle.top + gap || birdY + birdHeight > canvas.height - groundHeight)
+    ) {
+        return true; // Collision detected
+    }
+    return false; // No collision
+}
+
+function drawObstacles() {
+    for (var i = 0; i < obstacles.length; i++) {
+        var obs = obstacles[i];
+        ctx.drawImage(pipeTop, obs.x, 0, 50, obs.top);
+        ctx.drawImage(pipeBottom, obs.x, obs.top + gap, 50, canvas.height - (obs.top + gap + groundHeight));
+        if (!stopObstacles) {
+            obs.x -= obstacleSpeed;
+        }
+
+        if (checkCollision(birdX, birdY, birdWidth, birdHeight, obs) && !gameOver) {
+            // Collision detected
+            gameOver = true;
+            stopObstacles = true;
+        }
+
+        if (obs.x + 50 <= 0) {
+            obstacles.shift();
+        }
+    }
+}
+
+function generateObstacle() {
+    var min = 50;
+    var max = canvas.height - gap - groundHeight - 50;
+    var topHeight = Math.floor(Math.random() * (max - min + 1)) + min;
+    obstacles.push({ x: canvas.width, top: topHeight, width: 50 });
+}
+
+function drawBird() {
+    ctx.drawImage(bird, birdX, birdY, birdWidth, birdHeight);
+}
+
+
+
+function draw() {
+    if (gameStarted) {
+        velocity += gravity;
+        birdY += velocity;
+
+        if (birdY > canvas.height - birdHeight - groundHeight) {
+            birdY = canvas.height - birdHeight - groundHeight;
+            velocity = 0;
+            if (!gameOver) {
+                gameOver = true;
+                stopObstacles = true;
+            }
+        } else if (birdY < 0) {
+            birdY = 0;
+            velocity = 0;
+        }
+
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        drawBird();
+
+
+
+        drawObstacles();
+        if (!gameOver && (obstacles.length === 0 || obstacles[obstacles.length - 1].x < canvas.width - 300)) {
+            generateObstacle();
+        }
+    } else {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        drawBird();
+        ctx.fillStyle = "lightgreen";
+        ctx.font = "30px Arial";
+
+
+        ctx.fillText("Tap or Spacebar to start", canvas.width / 2 - 150, 40);
+    }
+
+    // Draw ground
+    ctx.fillStyle = "#8B4513";
+    ctx.fillRect(0, canvas.height - groundHeight, canvas.width, groundHeight);
+
+    if (gameOver) {
+        ctx.fillStyle = "red";
+        ctx.font = "30px Arial";
+        ctx.fillText("Game Over", canvas.width / 2 - 80, 80);
+        ctx.fillStyle = "black";
+        ctx.font = "20px Arial";
+        ctx.fillText("Tap or Spacebar to restart", canvas.width / 2 - 120, 120);
+    }
+
+    requestAnimationFrame(draw);
+}
+
+resizeCanvas();
